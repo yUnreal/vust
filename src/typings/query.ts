4145f8ca72e4188e066cf3ccda72dd0dbfcb5bcd
Unreal <unrealwebdev@gmail.com>
2024-06-25 14:18:37 -0300

    refactor(QueryOperatorBased): better query type

--- src/typings/query.ts
+++ src/typings/query.ts
@@ -21,6 +21,19 @@ export enum QueryOperators {
     //#endregion
 }
 
+export interface QueryOperatorBased<D extends AnyObject> {
+    [QueryOperators.Equal]?: DeepPartial<D>;
+    [QueryOperators.NotEqual]?: DeepPartial<D>;
+    [QueryOperators.Comment]?: string;
+    [QueryOperators.Where]?: WhereQueryFn<D>;
+    [QueryOperators.Greater]?: Partial<Pick<D, Find<D, number>>>;
+    [QueryOperators.Less]?: Partial<Pick<D, Find<D, number>>>;
+    [QueryOperators.GreaterThan]?: Partial<Pick<D, Find<D, number>>>;
+    [QueryOperators.LessThan]?: Partial<Pick<D, Find<D, number>>>;
+    [QueryOperators.Pattern]?: PartialRecord<Find<D, string>, RegExp>;
+    [QueryOperators.Length]?: PartialRecord<Find<D, string>, number>;
+}
+
 export interface QueryOptions<D extends AnyObject> {
     skip?: number;
     projection?: Projection<D>;
@@ -52,10 +65,5 @@ export enum UpdateOperators {
 }
 
 export type Projection<D extends AnyObject> = PartialRecord<keyof D, boolean>;
-
-export type QueryOperatorBased<D extends AnyObject> = PartialRecord<
-    Exclude<QueryOperators, QueryOperators.Where>,
-    DeepPartial<D>
->;
 
 export type WhereQueryFn<D extends AnyObject> = (data: D) => unknown;
