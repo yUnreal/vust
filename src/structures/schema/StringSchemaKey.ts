5a7dbe9870cedb07d30e001f261ec32e36481a49
Unreal <unrealwebdev@gmail.com>
2024-06-25 20:27:15 -0300

    feat(S.string): adding new methods

--- src/structures/schema/StringSchemaKey.ts
+++ src/structures/schema/StringSchemaKey.ts
@@ -1,4 +1,40 @@
-import { SchemaType } from '../../typings/schema';
+import { EffectError, SchemaType } from '../../typings/schema';
 import { SchemaKey } from './SchemaKey';
 
-export class StringSchemaKey extends SchemaKey<SchemaType.String> {}
+export class StringSchemaKey extends SchemaKey<SchemaType.String> {
+    public min(
+        value: number,
+        { message }: EffectError = {
+            message: `Min string length is "${value}"`,
+        }
+    ) {
+        return this.effect((string) => string.length >= value, message);
+    }
+
+    public max(
+        value: number,
+        { message }: EffectError = {
+            message: `Max string length is "${value}"`,
+        }
+    ) {
+        return this.effect((string) => string.length <= value, message);
+    }
+
+    public length(
+        length: number,
+        { message }: EffectError = {
+            message: `String length is "${length}"`,
+        }
+    ) {
+        return this.effect((string) => string.length === length, message);
+    }
+
+    public pattern(
+        regex: RegExp,
+        { message }: EffectError = {
+            message: `String must match pattern "${regex.source}"`,
+        }
+    ) {
+        return this.effect((string) => regex.test(string), message);
+    }
+}
